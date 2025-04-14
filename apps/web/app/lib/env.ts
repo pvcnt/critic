ed74cf22f98d456f134807346df869fa25654c47
Vincent <vincent.primault@gmail.com>
2025-04-14 16:45:13 +0200

    fix: prefix Vite environment variables

--- apps/web/app/lib/env.ts
+++ apps/web/app/lib/env.ts
@@ -3,8 +3,8 @@ import { z } from "zod";
 // Environment variables that are available both at build and runtime.
 // https://vercel.com/docs/environment-variables/system-environment-variables
 const EnvSchema = z.object({
-  VERCEL_GIT_COMMIT_SHA: z.string().optional(),
-  VERCEL_GIT_COMMIT_REF: z.string().optional(),
+  VITE_VERCEL_GIT_COMMIT_SHA: z.string().optional(),
+  VITE_VERCEL_GIT_COMMIT_REF: z.string().optional(),
 });
 
 export const env = EnvSchema.parse(process.env);
