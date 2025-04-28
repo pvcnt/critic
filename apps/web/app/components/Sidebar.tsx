08abb8d75ab943d74fb92f0153ee8f45e0a76f50
Vincent <vincent.primault@gmail.com>
2025-04-28 16:42:04 +0200

    feat: add a link to create a GitHub issue

--- apps/web/app/components/Sidebar.tsx
+++ apps/web/app/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { Button, Icon, Tooltip } from "@blueprintjs/core";
+import { AnchorButton, Button, Icon, Tooltip } from "@blueprintjs/core";
 import { NavLink } from "react-router";
 import { type BlueprintIcons_16Id } from "@blueprintjs/icons/lib/esm/generated/16px/blueprint-icons-16";
 import classes from "./Sidebar.module.scss";
@@ -41,14 +41,24 @@ export default function Sidebar({ login, isDark, onDarkChange }: SidebarProps) {
       <SidebarLink link="/settings" title="Settings" icon="cog" />
 
       <div className={classes.bottom}>
+        <Tooltip content="Report an issue or request a feature">
+          <AnchorButton
+            variant="minimal"
+            href="https://github.com/pvcnt/critic/issues/new"
+          >
+            <Icon icon="help" />
+          </AnchorButton>
+        </Tooltip>
         <Tooltip content={"Switch to " + (isDark ? "light" : "dark") + " mode"}>
           <Button onClick={onDarkChange} variant="minimal">
             <Icon icon={isDark ? "flash" : "moon"} />
           </Button>
         </Tooltip>
-        <Tooltip content={`Logout (${login || "Anonymous"})`}>
-          <SidebarLink link="/logout" title="Logout" icon="log-out" />
-        </Tooltip>
+        <SidebarLink
+          link="/logout"
+          title={`Logout (${login || "Anonymous"})`}
+          icon="log-out"
+        />
       </div>
     </div>
   );
